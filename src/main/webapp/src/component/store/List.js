94fa0b19297788a20ad57baa023324c9cdf06d12
leeyongshik <jk1405@naver.com>
2023-01-04 14:49:22 +0900

    스토어 헤드 수정

--- src/main/webapp/src/component/store/List.js
+++ src/main/webapp/src/component/store/List.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import styles from '../../css/List.module.css';
-import { BrowserRouter, Router, Link, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Router, Link, Route, Routes, useNavigate } from "react-router-dom";
 import Index from './Index';
 import Popcorn from './Popcorn';
 import Drink from './Drink';
@@ -8,9 +8,14 @@ import Snack from './Snack';
 import Combo from './Combo';
 import StoreHeader from './StoreHeader';
 import topButtonStyles from '../../css/TopButton.module.css';
+import axios from 'axios';
 
 const List = () => {
     const [showButton, setShowButton] = useState(false);
+    const [combo, setCombo] = useState(false)
+    const [popcorn, setPopcorn] = useState(false)
+    const [drink, setDrink] = useState(false)
+    const [snack, setSnack] = useState(false)
 
     const scrollToTop = (e) => {
         e.preventDefault()
@@ -21,6 +26,16 @@ const List = () => {
     }
 
     useEffect(() => {
+        setCombo(false)
+        setPopcorn(false)
+        setDrink(false)
+        setSnack(false)
+
+        axios.get(`http://localhost:8080/store/getCartList?userName=${sessionStorage.getItem("userName")}`)
+         .then(res => setCount(res.data.length))
+         .catch(error => console.log(error))
+
+
         const ShowButtonClick = () => {
             if (window.scrollY > 250) {
                 setShowButton(true)
@@ -34,7 +49,43 @@ const List = () => {
             window.removeEventListener("scroll", ShowButtonClick)
         }
     }, [])
-
+    const navigate = useNavigate()
+    const gotoIndex = () => {
+        setCombo(false)
+        setPopcorn(false)
+        setDrink(false)
+        setSnack(false)
+        navigate('/store/')   
+    }
+    const gotoCombo = () => {
+        setCombo(true)
+        setPopcorn(false)
+        setDrink(false)
+        setSnack(false)
+        navigate('/store/combo')
+    }
+    const gotoPopcorn = () => {
+        setCombo(false)
+        setPopcorn(true)
+        setDrink(false)
+        setSnack(false)
+        navigate('/store/popcorn')
+    }
+    const gotoDrink = () => {
+        setCombo(false)
+        setPopcorn(false)
+        setDrink(true)
+        setSnack(false)
+        navigate('/store/drink')
+    }
+    const gotoSnack = () => {
+        setCombo(false)
+        setPopcorn(false)
+        setDrink(false)
+        setSnack(true)
+        navigate('/store/snack')
+    }
+    const [count, setCount] = useState(0)
     return (
         <div >
             <div className={styles.big_banner_wrap}>
@@ -42,9 +93,9 @@ const List = () => {
                     <div className="bx-viewport" style={{width: '100%', overflow: 'hidden', position: 'relative', height: '400px'}}>
                         <ul className="bxslider" style={{width: 'auto', position: 'relative'}}>
                             <li style={{float: 'none', listStyle: 'none' , position: 'absolute', width: '980px', display: 'block'}}>
-                                <Link to='/store'>
+                                <a onClick={ gotoIndex } style={{cursor:'pointer'}}>
                                     <img src="../img/store2.jpeg" alt="기프트샵" style={{width:'980px',height:'400px', margin:'auto'}}/> 
-                                </Link>
+                                </a>
                             </li>
                         </ul>
                     </div>
@@ -55,7 +106,33 @@ const List = () => {
 
 
 
-            <StoreHeader/>
+            <div className={styles.category_wrap}>
+                <div className={styles.category_contents_wrap}>
+                    <ul className={styles.category_content} style={{listStyle: 'none'}}>
+                        <li id="cm4" name="categorymenu" className={combo  ? styles.active : ""}>
+                            <a onClick={ gotoCombo } style={{cursor:'pointer'}}>콤보</a>
+                            |
+                        </li>
+                        <li id="cm5" name="categorymenu" className={popcorn  ? styles.active : ""}>
+                            <a  href="#" onClick={ gotoPopcorn }>팝콘</a>
+                            |
+                        </li>
+                        <li id="cm6" name="categorymenu" className={drink  ? styles.active : ""}>
+                            <a onClick={ gotoDrink } style={{cursor:'pointer'}}>음료</a>
+                            |
+                        </li>
+                        <li id="cm7" name="categorymenu" className={snack  ? styles.active : ""}>
+                            <a onClick={ gotoSnack } style={{cursor:'pointer'}}>스낵</a>
+                        </li>
+                    </ul>
+                    <ul className={styles.cart_content} style={{listStyle: 'none'}}>
+                        <li>
+                            <a href="#">장바구니</a>
+                            <span id="cartviewcnt">{ count }</span>
+                        </li>
+                    </ul>
+                </div>
+            </div>
             
 
 
--- src/main/webapp/src/component/store/StoreHeader.js
+++ src/main/webapp/src/component/store/StoreHeader.js
@@ -1,35 +1,68 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import styles from '../../css/List.module.css'
 
 const StoreHeader = () => {
     const [count, setCount] = useState(0)
+    const [combo, setCombo] = useState(false)
+    const [popcorn, setPopcorn] = useState(false)
+    const [drink, setDrink] = useState(false)
+    const [snack, setSnack] = useState(false)
     useEffect(() => {
         axios.get(`http://localhost:8080/store/getCartList?userName=${sessionStorage.getItem("userName")}`)
          .then(res => setCount(res.data.length))
          .catch(error => console.log(error))
     }, [])
+    const navigate = useNavigate()
+    const gotoCombo = () => {
+        setCombo(true)
+        setPopcorn(false)
+        setDrink(false)
+        setSnack(false)
+        navigate('/store/combo')
+    }
+    const gotoPopcorn = () => {
+        setCombo(false)
+        setPopcorn(true)
+        setDrink(false)
+        setSnack(false)
+        navigate('/store/popcorn')
+    }
+    const gotoDrink = () => {
+        setCombo(false)
+        setPopcorn(false)
+        setDrink(true)
+        setSnack(false)
+        navigate('/store/drink')
+    }
+    const gotoSnack = () => {
+        setCombo(false)
+        setPopcorn(false)
+        setDrink(false)
+        setSnack(true)
+        navigate('/store/snack')
+    }
 
     return (
         <>
             <div className={styles.category_wrap}>
                 <div className={styles.category_contents_wrap}>
                     <ul className={styles.category_content} style={{listStyle: 'none'}}>
-                        <li id="cm4" name="categorymenu" className={ styles.active }>
-                            <Link to='/store/combo'>콤보</Link>
+                        <li id="cm4" name="categorymenu" className={combo  ? styles.active : ""}>
+                            <a onClick={ gotoCombo } style={{cursor:'pointer'}}>콤보</a>
                             |
                         </li>
-                        <li id="cm5" name="categorymenu" className="">
-                            <Link to='/store/popcorn'>팝콘</Link>
+                        <li id="cm5" name="categorymenu" className={popcorn  ? styles.active : ""}>
+                            <a  href="#" onClick={ gotoPopcorn }>팝콘</a>
                             |
                         </li>
-                        <li id="cm6" name="categorymenu" className="">
-                            <Link to='/store/drink'>음료</Link>
+                        <li id="cm6" name="categorymenu" className={drink  ? styles.active : ""}>
+                            <a onClick={ gotoDrink } style={{cursor:'pointer'}}>음료</a>
                             |
                         </li>
-                        <li id="cm7" name="categorymenu" className="">
-                            <Link to='/store/snack'>스낵</Link>
+                        <li id="cm7" name="categorymenu" className={snack  ? styles.active : ""}>
+                            <a onClick={ gotoSnack } style={{cursor:'pointer'}}>스낵</a>
                         </li>
                     </ul>
                     <ul className={styles.cart_content} style={{listStyle: 'none'}}>

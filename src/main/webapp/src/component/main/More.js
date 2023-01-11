b917cd23a02c88d994867785ce6ced134f198a08
leeyongshik <jk1405@naver.com>
2023-01-11 13:17:46 +0900

    11

--- src/main/webapp/src/component/main/More.js
+++ src/main/webapp/src/component/main/More.js
@@ -5,6 +5,18 @@ import Shoes1 from './data1.json';
 const More = () => {
     let [ shoes, setShoes ] = useState(Shoes1);	
     let [ pageIdx, setPageIdx ]= useState(2);
+
+    const more = () => {
+      axios.get('/data1.json')
+           .then((resp) => {
+              setShoes([
+                ...shoes,
+                ...resp.data
+              ]);
+              setPageIdx(pageIdx + 1)})
+           .catch((err) => console.log(err))
+    }
+    console.log(pageIdx)
    
     return (
         <div>
@@ -13,38 +25,22 @@ const More = () => {
       <div className='container'>	
         <div className='row'>	
           {	
-            shoes.map((obj, i) => {	
-              return <Card key={i} i={i} shoes={shoes} />	
+            shoes.map((item, index) => {	
+              return (
+                <div className='col-md-4' key={index}>	
+                  <img src={ item.image } alt="신발" width="100px" />	
+                  <h3>{ item.title }</h3>	
+                  <p>{ item.content } & { item.price }</p>	
+                </div>	
+              )	
             })	
           }	
         </div>
       </div>
           
-      <a class="btn btn-secondary btn" role="button" onClick={() => {
-        axios.get('/data1.json').then((resp) => {
-          setShoes([
-            ...shoes,
-            ...resp.data
-          ]);
-          setPageIdx(pageIdx + 1);
-        }).catch((e) => {
-          console.log(e);
-        });
-      }}>더보기</a>
+      <a className="btn btn-secondary btn" role="button" onClick={ more } style={{cursor:'pointer'}}>더보기</a>
         </div>	
   );	
 }	
-
-function Card(props) {	
-    return (	
-      <>	
-        <div className='col-md-4'>	
-          <img src={ props.shoes[props.i].image } alt="신발" width="100px" />	
-          <h3>{ props.shoes[props.i].title }</h3>	
-          <p>{ props.shoes[props.i].content } & { props.shoes[props.i].price }</p>	
-        </div>	
-      </>	
-    )	
-  }	
 
 export default More;
